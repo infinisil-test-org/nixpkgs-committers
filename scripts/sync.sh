0d24d4f478115382b7bdfaf20eb7de76d0a1a82a
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-06-13 01:26:44 +0200

    Initial commit

---
+++ scripts/sync.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
+  echo >&2 "Usage: $0 ORG TEAM DIR"
+  exit 1
+}
+
+ORG=${1:-$(usage)}
+TEAM=${2:-$(usage)}
+DIR=${3:-$(usage)}
+
+shopt -s nullglob
+
+for file in "$DIR"/*; do
+  rm "$file"
+done
+
+mkdir "$DIR"
+
+gh api /orgs/"$ORG"/teams/"$TEAM"/members --paginate --jq '.[].login' |
+  while read -r login; do
+    touch "$DIR"/"$login"
+  done
